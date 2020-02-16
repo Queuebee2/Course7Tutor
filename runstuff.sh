afbc2a322cec54f6039dc9c58576b6bccbba77dc
Milain <32010930+Queuebee2@users.noreply.github.com>
2020-02-16 16:27:22 +0100

    Update runstuff - make it easier to change filenames
    Maybe transfer to args later

--- runstuff.sh
+++ runstuff.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+REF_GENOME_FILE="SD.fa"
+READS_FILE1="SD_3_1_400bp.fq"
+READS_FILE2="SD_3_2_400bp.fq"
+
 printf "be aware that to use this script, Xming or x11 or something must be running for IGV to start at the end\n"
 sleep 3
-printf "Creating indexes from reference genome ("SD.fa") with bowtie2-build\n" &&
-bowtie2-build "SD.fa" SD &&
+printf "Creating indexes from reference genome ($REF_GENOME_FILE) with bowtie2-build\n" &&
+bowtie2-build "$REF_GENOME_FILE" SD &&
 printf "Indexes successfully made\n" &&
-printf "Creating a sequence alignment map with bowtie2  \n" &&
-bowtie2 --phred64 --very-fast --no-unal -x SD -1 SD_3_1_400bp.fq -2 SD_3_2_400bp.fq -S Docc.sam &&
+printf "Creating a sequence alignment map with bowtie2 -- output name: $OUTPUTFILENAME \n" &&
+bowtie2 --phred64 --very-fast --no-unal -x SD -1 "$READS_FILE1" -2 "$READS_FILE2" -S Docc.sam &&
 printf "successfully ran bowti2\n" || printf "Bowtie2 failed. if the error is something something 134 then thats intended\n" &&
 printf "creating view with samtools\n" &&
 samtools view -b -o Docc.sort.bam -S Docc.sam &&
